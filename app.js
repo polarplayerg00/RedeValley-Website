2b0636cc8581d02b94ca8b5043c2421aabecd5d2
yPolar <149212710+polarplayerg00@users.noreply.github.com>
2024-02-12 13:52:23 -0400

    Create app.js

---
+++ app.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const hamburger = document.querySelector('.hamburger');
+  const navList = document.querySelector('.nav-list');
+
+  hamburger.addEventListener('click', () => {
+    navList.classList.toggle('active');
+  });
+}); 
